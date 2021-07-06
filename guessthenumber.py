56743666f779256bbc272637e66a2128c7b299be
harshitrmp <67100302+harshitrmp@users.noreply.github.com>
2021-07-06 21:11:42 +0530

    Add files via upload
    This is number guessing game.

---
+++ guessthenumber.py
@@ -0,0 +1,15 @@
+import random
+random_number = random.randint(0, 100)
+attempts = 0
+
+while True:
+    attempts += 1
+    guess = int(input('Enter your guess:\t'))
+    if guess == random_number:
+        break
+    elif guess < random_number:
+        print('Too low')
+    else:
+        print('Too high')
+
+print("Good job! You guessed the number in",attempts,"attempts")
